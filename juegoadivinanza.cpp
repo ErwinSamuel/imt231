23cae1afb869f50cdef18620fea8a63956103962
unknown <ErwinSamuel>
2024-09-12 12:22:55 -0400

    2024-09-12_01 primera version

---
+++ juegoadivinanza.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main(){
+std::cout<<"***************************************"<<std::endl;
+std::cout<<"*Bienvenido al juego de la Adivinanza!*"<<std::endl;
+std::cout<<"***************************************"<<std::endl;
+}
