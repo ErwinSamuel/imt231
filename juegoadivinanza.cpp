333687c0e9e68530edcacdd2cf0afe260e6ce91d
unknown <ErwinSamuel>
2024-09-24 09:02:35 -0400

    2024-09-24_01 controlar ciclo hata adivinar

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
 std::cout<<"***************************************"<<endl;
 std::cout<<"*Bienvenido al juego de la Adivinanza!*"<<endl;
 std::cout<<"***************************************"<<endl;
 
 const int NUMERO_SECRETO = 42;
-
 int adivina;
+
+bool no_acerto = true;
+while(no_acerto){
 
 cout << "¿Cual es el numero? " ;
 cin >> adivina;
 cout << "El valor de su  numero es: " << adivina << endl;
 
-bool acerto = adivina == NUMERO_SECRETO;
-bool mayor = adivina > NUMERO_SECRETO;
+ bool acerto = adivina == NUMERO_SECRETO;
+ bool mayor = adivina > NUMERO_SECRETO;
 
 if(acerto){
-cout << "felicitaciones!!! ... adivino el numero secfreto" << endl;
+  cout << "felicitaciones!!! ... adivino el numero secfreto" << endl;
+  no_acerto = false;
  }
 else if(mayor){
  cout << "EL numero ingresado es mayor que el numero secreto" << endl;
   }
  else{ 
  cout << "El numero incresado es menor que el numero secreto " <<endl; }
+}
+cout << "Finalizo el juego" << endl;
 }
