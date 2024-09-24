07eddb388372a232448b3916bd8bcac8ba4ea76f
unknown <ErwinSamuel>
2024-09-24 09:54:46 -0400

    2024-09-24_02

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -10,10 +10,15 @@ const int NUMERO_SECRETO = 42;
 int adivina;
 
 bool no_acerto = true;
+int intentos = 0;
+double puntos = 1000.0;
 while(no_acerto){
-
+ intentos ++;
+cout << "Tentativa " << intentos << endl;
 cout << "¿Cual es el numero? " ;
 cin >> adivina;
+double puntos_perdidos = abs(adivina - NUMERO_SECRETO)/2.0;
+puntos = puntos-puntos_perdidos;
 cout << "El valor de su  numero es: " << adivina << endl;
 
  bool acerto = adivina == NUMERO_SECRETO;
@@ -26,8 +31,13 @@ if(acerto){
 else if(mayor){
  cout << "EL numero ingresado es mayor que el numero secreto" << endl;
   }
- else{ 
- cout << "El numero incresado es menor que el numero secreto " <<endl; }
+ else{
+ cout << "El numero incresado es menor que el numero secreto " <<endl;
+ }
 }
 cout << "Finalizo el juego" << endl;
+cout << "Adivino el numero secreto en " << intentos << " intentos."  << endl;
+cout.precision(2);
+cout << fixed;
+cout << "Su puntuacion fue de " << puntos << " puntos " << endl;
 }
