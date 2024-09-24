b3217604873e1eb9aca3572c2e407d175764a2c5
unknown <ErwinSamuel>
2024-09-24 08:28:20 -0400

    2024-09-24 aplicasion de bool

--- juegoadivinanza.cpp
+++ juegoadivinanza.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 using namespace std;
 int main(){
+std::cout<<"***************************************"<<endl;
+std::cout<<"*Bienvenido al juego de la Adivinanza!*"<<endl;
+std::cout<<"***************************************"<<endl;
 
-cout<<"**************************************"<<endl;
-
-cout<<"*Bienvenido al JUEGO de la adivinanza!*"<<endl;
-cout<<"***************************************"<<endl;
-
-int numero_secreto = 42;
-cout<<"el numero secreto es..."<<numero_secreto<<". No lo digas a nadie!"<<endl;
+const int NUMERO_SECRETO = 42;
 
 int adivina;
-cin>>adivina;
-cout<<"El valor de su numero es: "<<adivina<<endl;
 
-if(adivina == numero_secreto){
-	cout << "Felicitaciones!!...adivinó el numero secreto" <<endl;
-	}
-else if(adivina > numero_secreto){
-	cout<< "El numero ingresado es mayor que el numero secreto"<<endl;
-	}
-else{
-	cout<<"El numero ingresado es menor que el numero secreto"<<endl;
-	}
+cout << "¿Cual es el numero? " ;
+cin >> adivina;
+cout << "El valor de su  numero es: " << adivina << endl;
 
+bool acerto = adivina == NUMERO_SECRETO;
+bool mayor = adivina > NUMERO_SECRETO;
+
+if(acerto){
+cout << "felicitaciones!!! ... adivino el numero secfreto" << endl;
+ }
+else if(mayor){
+ cout << "EL numero ingresado es mayor que el numero secreto" << endl;
+  }
+ else{ 
+ cout << "El numero incresado es menor que el numero secreto " <<endl; }
 }
